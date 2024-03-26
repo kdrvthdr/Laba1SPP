59e3b18d50c957ffd6dd79b36e11c86ce31b4c6f
kdrvthdr <90689465+kdrvthdr@users.noreply.github.com>
2024-03-26 19:47:44 +0300

    lab1

---
+++ lab1/Foo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Foo
+    {
+        private readonly Bar _bar;
+        private readonly ITracer _tracer;
+
+        public Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(tracer);
+        }
+
+        public void MyMethod()
+        {
+            _tracer.StartTrace(); // Начало трассировки
+
+            // Логика вашего метода MyMethod
+            Console.WriteLine("MyMethod is executing...");
+
+            // Пример какой-то длительной операции в методе MyMethod
+            for (int i = 0; i < 1000000; i++)
+            {
+                // Просто замедляем выполнение, чтобы увидеть результаты трассировки
+                int result = i * i;
+            }
+
+            _bar.InnerMethod(); // Вызов другого метода, который также будет трассироваться
+
+            Console.WriteLine("MyMethod execution completed.");
+
+            _tracer.StopTrace(); // Окончание трассировки
+        }
+    }
+
+    public class Bar
+    {
+        private readonly ITracer _tracer;
+
+        public Bar(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void InnerMethod()
+        {
+            _tracer.StartTrace();
+            // Логика выполнения метода InnerMethod
+            System.Threading.Thread.Sleep(100); // Приостановка выполнения потока на 100 миллисекунд для имитации работы метода
+            _tracer.StopTrace();
+        }
+    }
+}
---
+++ lab1/Tracer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace lab1
+{
+    [Serializable]
+    public class TraceResult
+    {
+        public string MethodName { get; set; }
+        public string ClassName { get; set; }
+        public string Time { get; set; }
+        public List<TraceResult> Methods { get; } = new List<TraceResult>();
+    }
+
+    public interface ITracer
+    {
+        // вызывается в начале замеряемого метода
+        void StartTrace();
+
+        // вызывается в конце замеряемого метода 
+        void StopTrace();
+
+        // получить результаты измерений  
+        TraceResult GetTraceResult();
+    }
+
+    public class Tracer : ITracer
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        public  Stack<TraceResult> _methodStack = new Stack<TraceResult>();
+        private TraceResult _rootResult;
+
+        public void StartTrace()
+        {
+            var methodInfo = GetCallingMethodInfo();
+            var traceResult = new TraceResult
+            {
+                MethodName = methodInfo.MethodName,
+                ClassName = methodInfo.ClassName
+            };
+
+            if (_methodStack.Count == 0)
+            {
+                _rootResult = traceResult;
+            }
+            else
+            {
+                var parent = _methodStack.Peek();
+                parent.Methods.Add(traceResult);
+            }
+
+            _methodStack.Push(traceResult);
+
+            _stopwatch.Start();
+        }
+
+        public void StopTrace()
+        {
+            _stopwatch.Stop();
+            var elapsedMilliseconds = _stopwatch.ElapsedMilliseconds;
+
+            var completedMethod = _methodStack.Pop();
+            completedMethod.Time = elapsedMilliseconds + "ms";
+
+            _stopwatch.Reset();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return _rootResult;
+        }
+
+        private (string MethodName, string ClassName) GetCallingMethodInfo()
+        {
+            var stackTrace = new StackTrace();
+            var callingFrame = stackTrace.GetFrame(2);
+            var method = callingFrame.GetMethod();
+            var methodName = method.Name;
+            var className = method.DeclaringType.Name;
+
+            return (methodName, className);
+        }
+    }
+
+  
+}
